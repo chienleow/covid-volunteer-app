3831c024563f978ddd774040af931ad0f3402110
chienleow <chienleow1@gmail.com>
2020-07-09 17:01:56 -0400

    seeding data

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# (mock data for my program)
+
+# ------------------ Users ------------------ 
+
+# ------------------  Local businesses ------------------ 
+peace_coffee = User.create(name: "Peace Coffee", description: "A bean roastery and local coffee chain that supports the Institute for Agriculture and Trade Policy.", email: "peacecoffee@gmail.com", password: "coffeebean")
+
+rabbit_hole = User.create(name: "The Rabbit Hole", description: "A Korean "Alice in Wonderland"-inspired restaurant.", email: "therabbithole@gmail.com", password: "koreanfood")
+
+wild_rumpus = User.create(name: "Wild Rumpus", description: "A children's bookstore that lives the stories it sells.", email: "wildrumpus@gmail.com", password: "readmorebooks")
+
+# ------------------ Local non-profits ------------------ 
+neighborhood_house = User.create(name: "Neighborhood House", description: "We are a multi-service agency, providing wrap-around services for people when they need it most.", email: "neighborhoodhouse@gmail.com", password: "goodneighbor")
+
+fin = User.create(name: "Friends In Need", description: "Provide food and resources to those in need.", email: "fin@gmail.com", password: "friends")
+
+salvation_army = User.create(name: "The Salvation Army Saint Paul", description: "Wherever there is a need in western Saint Paul, you'll find the Salvation Army.", email: "salvationarmy@gmail.com", password: "lovepeople")
+
+# ------------------ Projects ------------------ 
+Project.create(title: "Social Media", image_url: "", information: "REMOTE: Peace Coffee would love help for setting up a Facebook page to showcase online menu and pickup/delivery options.", user_id: peace_coffee.id)
+Project.create(title: "Online Order", image_url: "", information: "REMOTE: Rabbit Hole would appreciate help for setting up online order and delivery on the website.", user_id: rabbit_hole.id)
+Project.create(title: "Book Organizer", image_url: "", information: "IN-PERSON: Wild Rumpus is looking for 2 volunteers to help organizing books and managing new pickup option.", user_id: wild_rumpus.id)
+Project.create(title: "Food Bank", image_url: "", information: "IN-PERSON: Neighborhood House needs 15 volunteers to help food distribution.", user_id: neighborhood_house.id)
+Project.create(title: "Produce Giveaway", image_url: "", information: "IN-PERSON: Friends In Need is looking for help to collect and giveaway produce.", user_id: fin.id)
+Project.create(title: "Inventory Management", image_url: "", information: "REMOTE: The Salvation Army Saint Paul is seeking a volunteer to help set up inventory management online.", user_id: salvation_army.id)
