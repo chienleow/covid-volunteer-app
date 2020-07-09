3bee93f20314d308f84efbf4898adbf2f77b5e3e
chienleow <chienleow1@gmail.com>
2020-07-09 16:15:20 -0400

    migrated tables

--- db/migrate/20200709040815_create_users.rb
+++ db/migrate/20200709040815_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-        t.string :email
-        t.string :password_digest
         t.string :name
         t.text :description
+        t.string :email
+        t.string :password_digest
+
+        t.timestamps null: false
     end
   end
 end
