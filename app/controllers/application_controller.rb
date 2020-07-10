34cbafe0abe8f8b39bd160d07e22ce5b5970ccb8
chienleow <chienleow1@gmail.com>
2020-07-09 23:36:10 -0400

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enables the sessions hash
+    enable :sessions
+    # set a session secret for an extra layer of security
+    set :session_secret, "secret_session"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,20 @@ class UsersController < ApplicationController
         # find the user
         user = User.find_by(email: params[:email]) # pull data from the params
         # authenticate the user
-        if user.authenticate(params[:password]) # from has_secure_password macro
-            # log them in
+        if user && user.authenticate(params[:password]) # .authenticate from has_secure_password macro
+            # 'if user' make sure user can be found first then authenticate them
+            # creating a 'key/value pair' in the session hash using the user's id to log them in
+            session[:user_id] = user.id
             # redirect logged_in user to user's profile
+            redirect "users/#{user.id}"
         else
             # show an error message
             redirect '/login'
         end
+    end
+
+    get '/users/:id' do
+
     end
 
 end
