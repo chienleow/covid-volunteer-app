95d592e720228b7eaa2dd062d7679347a759f451
chienleow <chienleow1@gmail.com>
2020-07-10 17:20:08 -0400

    show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
     # receive the data (params) from the login form
     post '/login' do
         # find the user
-        user = User.find_by(email: params[:email]) # pull data from the params
+        @user = User.find_by(email: params[:email]) # pull data from the params
         # authenticate the user
-        if user && user.authenticate(params[:password]) # .authenticate from has_secure_password macro
+        if @user && @user.authenticate(params[:password]) # .authenticate from has_secure_password macro
             # 'if user' make sure user can be found first then authenticate them
             # creating a 'key/value pair' in the session hash using the user's id to log them in
-            session[:user_id] = user.id
+            session[:user_id] = @user.id
             # redirect logged_in user to user's profile
-            redirect "/users/#{user.id}"
+            redirect "/users/#{@user.id}"
         else
             # show an error message
             redirect '/login'
@@ -23,7 +23,8 @@ class UsersController < ApplicationController
     end
 
     get '/users/:id' do
-        "user show page"
+        @user = User.find_by(id: params[:id])
+        erb :'users/show'
     end
 
 end
