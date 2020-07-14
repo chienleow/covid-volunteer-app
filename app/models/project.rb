006a2fbcb45137caef8634c7df566ebdd67fbd92
chienleow <chienleow1@gmail.com>
2020-07-14 00:24:02 -0400

    validate input

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
     belongs_to :user
+
+    validates :title, :image_url, :information, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
-
+    validates :email, uniqueness: true
     has_many :projects
 end
