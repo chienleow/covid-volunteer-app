7d7133dcc628f7ba2aca9055da8089804f12c284
chienleow <chienleow1@gmail.com>
2020-07-15 23:11:01 -0400

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :email, uniqueness: true
+    validates :name, :description, :email, :password, presence: true
     has_many :projects
 end
